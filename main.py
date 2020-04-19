f5ed5a371bec66d36066be34941b0129c580279e
Daniel Lott <12213460+Dl0tt@users.noreply.github.com>
2020-04-19 16:06:00 -0300

    New API
    Fix #1 and make it work on both python2 and python3

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import requests
 import random
 
+# Making input version agnostic
+try:
+   input = raw_input
+except NameError:
+   pass
+
 # Generates a question and a response based on a given word
 # TODO recognition of gender | tell a little small talk introduction
 def gen(word):
-    resp = raw_input("Conhece o(a) %s? (S/N) " % word)
+    resp = input("Conhece o(a) %s? (S/N) " % word)
     if resp in ['S', 's']:
-        print ':('
+        print(':(')
     elif resp in ['N', 'n']:
-        print "Que " + find_expr(word)
+        print("Que " + find_expr(word))
 
 # Match an given word to an expression which rhymes
 # TODO map person names to things names (dictionary API? http://www.dicionario-aberto.net/estaticos/api.html )
 def find_expr(word):
-    URI = 'http://dicionario-aberto.net'
-    r = requests.get(URI + "/search-json?suffix=%s&like=%s" % (word[-4:], word))
-    l = r.json()['list']
+    URI = 'https://api.dicionario-aberto.net/suffix/%s?like=%s' % (word[-4:], word.lower())
+    r = requests.get(URI)
+    l = r.json()
     random.shuffle(l)
-    return 'encheu seu cu de ' + l[0]
+    return 'encheu seu cu de ' + l[0]['word']
 
 # x = raw_input('Digite um nome: ')
 # gen(x)
